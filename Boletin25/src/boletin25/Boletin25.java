bbc2475985c0d43b43ca84f48414508939403861
AlvaroLopez <alvarocobo14@gmail.com>
2017-06-05 19:28:27 +0200

    Initial Commit

---
+++ Boletin25/src/boletin25/Boletin25.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin25;
+
+/**
+ *
+ * @author alopezcobo
+ */
+public class Boletin25 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Ejer1 obxEjer1 = new Ejer1();
+        obxEjer1.setVisible(true);
+        Ejer2 obxEjer2 = new Ejer2();
+        obxEjer2.setVisible(true);
+        Ejer3 obxEjer3 = new Ejer3();
+        obxEjer3.setVisible(true);
+        Ejer4 obxEjer4 = new Ejer4();
+        obxEjer4.setVisible(true);
+    }
+    
+}
